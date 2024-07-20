85120b98dcdab74f39f57d1830b4180980ce7c46
Danda Pranav Krishna <pranavkrishna317@gmail.com>
2024-07-20 23:52:02 +0530

    Update settings.py

--- School_management_System/student_management_project/settings.py
+++ School_management_System/student_management_project/settings.py
@@ -35,7 +35,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 STATIC_URL="/static/"
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
